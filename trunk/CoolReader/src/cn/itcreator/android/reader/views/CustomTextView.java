5f3472b340b637662efc22e498473819c43ad1c8
mawei.81 <mawei.81@72fe2aae-c5f7-11dd-a86e-7346a4b24aab>
2009-04-22 09:45:00 +0000

    add custom view modules

---
+++ trunk/CoolReader/src/cn/itcreator/android/reader/views/CustomTextView.java
@@ -0,0 +1,28 @@
+package cn.itcreator.android.reader.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class CustomTextView extends View{
+
+	public CustomTextView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+			super.onDraw(canvas);
+
+		
+	}
+	
+	
+	
+	
+	
+
+}
